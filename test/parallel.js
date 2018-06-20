d8887ce45ab49fd9c695e296601d14bc4d586156
Mohammad Fares <faressoft.com@gmail.com>
2018-06-21 01:32:42 +0300

    Add tests empty files

---
+++ test/parallel.js
@@ -0,0 +1,9 @@
+/**
+ * Tests the parallel runner
+ * 
+ * @author Mohammad Fares <faressoft.com@gmail.com>
+ */
+
+var expect   = require('chai').expect;
+var parallel = require('../parallel.js');
+
--- test/series.js
+++ test/series.js
@@ -1,9 +1,9 @@
 /**
- * The main tests for Flowa
+ * Tests the series runner
  * 
  * @author Mohammad Fares <faressoft.com@gmail.com>
  */
 
 var expect = require('chai').expect;
-var Flowa  = require('../index.js');
+var series = require('../series.js');
 
